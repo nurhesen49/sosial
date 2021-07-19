cbb1c9e56f6fa60fea3cd6299fc70ce0b590cb35
nurhesen49 <39801158+nurhesen49@users.noreply.github.com>
2021-07-19 20:03:09 +0400

    Add files via upload

--- dostumqunduz/frontend/src/components/isauth/authpage/NavBar.js
+++ dostumqunduz/frontend/src/components/isauth/authpage/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component, useState } from "react";
 import Search from "../Search";
-
+import { icons } from "../Icons";
 
 export default function NavBar(props){
 
@@ -9,7 +9,7 @@ export default function NavBar(props){
 
     return(
             <div className="navbar">
-                <div className="brandname"></div>
+                <div className="brandname">{icons.qunduz_ikon}DostumQunduz</div>
                 <div className="searchbar"><Search /></div>
                 <div className="profilestuff"></div>
                 </div>
